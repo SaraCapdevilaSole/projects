79f21ad4f84459d1100a61089df2230bb1de3050
SaraCapdevilaSole <66724126+SaraCapdevilaSole@users.noreply.github.com>
2024-10-08 16:30:15 +0200

    Update balloon_model.py

--- MSc_thesis/Firedrake/src/utils/balloon_model.py
+++ MSc_thesis/Firedrake/src/utils/balloon_model.py
@@ -16,14 +16,6 @@ class Balloon:
         self.alpha = 0.32  # Grubb’s exponent
 
         if no_ODE_y:
-            # Parameters from Deco: https://github.com/decolab/cb-neuromod/blob/6fa5abb8226e2c15330b187bfadc1924dec84918/functions/BOLD.m#L47
-            # self.rho = 0.34 
-            # self.k1 = self.rho * 7    
-            # self.k2 = 2
-            # self.k3 = 2 * self.rho - 0.2
-            # self.V0 = 0.02   
-
-            # THIS WOKRS AS EXPECTED!
             self.rho = 0.4
             self.k1 = 4.3*40.3*self.rho*0.04
             self.k2 = 25*self.rho*0.04
@@ -38,14 +30,7 @@ class Balloon:
             self.V0 = 0.02     # resting blood volume fraction
 
         self.t_array = t_array
-        # self.data_path_f: callable = lambda f: data_path + f'_{f}.vtu'
         self.data = self._format_data(data)
-
-    # def _load_data(self):
-    #     logging.info(f'Loading files from path: {self.data_path_f('file_#')}')
-    #     return {
-    #         t: self._load_file(self.data_path_f(t)) for t in tqdm(range(0,len(self.t_array)))
-    #     }
 
     def _format_data(self, data):
         logging.info(f'Formating input files')
@@ -57,12 +42,6 @@ class Balloon:
             logging.error('Data should contain time and phi_e information')
         except TypeError:
             logging.error('Time array should be element 0, and phi_e element 1')
-
-    # @staticmethod
-    # def _load_file(data_path):
-    #     mesh = meshio.read(data_path)
-    #     mesh_phi_e = mesh.point_data['phi_e']
-    #     return mesh_phi_e
 
     def get_n_parcellations(self) -> int:
         return len(self.data[next(iter(self.data))])
@@ -76,8 +55,6 @@ class Balloon:
             return np.abs(self.t_array - t_i).argmin()
 
     def ode_system(self, t, x: np.array):
-        # x = [z,f,v,q,y]
-        # x size: [num vertices, 5] 
         function_names = ['_dz_dt', '_df_dt', '_dv_dt', '_dq_dt', '_dy_dt']
         derivatives = [getattr(self, func)(t, x) for func in function_names]
         return np.array(derivatives)
